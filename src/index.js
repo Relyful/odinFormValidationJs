05dca8143a42e40c81cf7920beac8cf9e4079c6e
Relyful <13381270+Relyful@users.noreply.github.com>
2024-04-17 21:49:03 +0200

    Create basic html form and create event listener for email input and regex check on input

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log('This is a test.');
+import './style.css';
+
+
+const email = document.querySelector('#email');
+
+
+const emailRegEx = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+
+email.addEventListener('input', (event) => {
+    if (emailRegEx.test(email.value)) {
+        console.log('Yahooooooo');
+        email.setCustomValidity('');
+    } 
+    else {
+        email.setCustomValidity('TO ses posral');
+        email.reportValidity();
+        console.log(`D'Oh!`);
+    }
+})
